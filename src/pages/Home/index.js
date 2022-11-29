bbc57aa7068f4b1d9bbf212335185e2daef51f75
Brendan-Geoghegan <brendangeoghegan@outlook.com>
2022-11-29 17:35:03 +0000

    home page functionality added

---
+++ src/pages/Home/index.js
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from 'react'
+import axios from "axios"
+
+
+
+export default function Home() {
+
+    const [ longUrl, setLongUrl ] = useState("")
+    const [ shortUrl, setShortUrl ] = useState("")
+
+    // useEffect(() => {
+
+    // }, [shortUrl])
+
+    const getShortUrl = async (url) => {
+        const urldata = await axios.post('http://127.0.0.1:8000/urls/create/', {long_url: url})
+        setShortUrl(urldata.data.fields.short_url)
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault()
+        getShortUrl(longUrl)
+    }
+
+    const handleChange = e => {
+        setLongUrl(e.target.value)
+    }
+
+    return (
+    <div>
+        <form onSubmit={handleSubmit}>
+            <input type="text" name="long_url" onChange={handleChange}/>
+            <input type="submit" value="Submit"/>
+        </form>
+        <p>Short url: <a href={shortUrl}>{shortUrl}</a></p>
+    </div>
+    )
+}
