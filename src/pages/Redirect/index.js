81346f0a6d89741336c9996ac7f07be15bcdaf26
Brendan-Geoghegan <brendangeoghegan@outlook.com>
2022-11-30 09:37:41 +0000

    paths with no long url redirect to home

--- src/pages/Redirect/index.js
+++ src/pages/Redirect/index.js
@@ -1,17 +1,21 @@
 import React, { useEffect } from 'react'
-import { redirect, useNavigate, useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 import axios from "axios"
 
 export default function Redirect() {
 
     const { shortUrl } = useParams()
-    console.log(shortUrl);
     const navigate = useNavigate()
 
     const getLongUrl = async (url) => {
-        const urldata = await axios.get(`http://127.0.0.1:8000/urls/${url}`)
-        console.log(urldata.data.fields.long_url);
-        window.location.assign(urldata.data.fields.long_url)
+        try {
+            const urldata = await axios.get(`http://127.0.0.1:8000/urls/${url}`)
+            console.log(urldata);
+            if(urldata.data.fields.long_url)
+            window.location.assign(urldata.data.fields.long_url)
+        } catch (error) {
+            navigate("/")
+        }
     }
     
     useEffect(() => {
